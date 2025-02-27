e6f9e1240c6e8c771416eed7b264a068532cfc25
Dean Eckert <dean.eckert@red-oak-consulting.com>
2025-02-27 15:37:45 +0100

    home-manager: nixneovim: fixed cmd config

--- modules/home-manager/nixneovim.nix
+++ modules/home-manager/nixneovim.nix
@@ -54,49 +54,22 @@
         enable = true;
         snippet.luasnip.enable = true;
         completion = {
-          keyword_length = 1;
-          keyword_pattern = ".*";
+          completeopt = "menu,menuone,preview,noselect";
         };
         mapping = {
-          "<Tab>" = ''
-            cmp.mapping(function(fallback)
-              local has_words_before = function()
-                unpack = unpack or table.unpack
-                local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-                return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-              end
-
-              if cmp.visible() then
-                cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
-              -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
-              -- they way you will only jump inside the snippet region
-              elseif has_words_before() then
-                cmp.complete()
-              else
-                fallback()
-              end
-            end, { "i", "s" })
-          '';
-          "<S-Tab>" = ''
-            cmp.mapping(function(fallback)
-              local has_words_before = function()
-                unpack = unpack or table.unpack
-                local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-                return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-              end
-
-              if cmp.visible() then
-                cmp.select_prev_item()
-              else
-                fallback()
-              end
-            end, { "i", "s" })
-          '';
+          "<C-k>" = "cmp.mapping.select_prev_item()";
+          "<Up>" = "cmp.mapping.select_prev_item()";
+          "<C-j>" = "cmp.mapping.select_next_item()";
+          "<Down>" = "cmp.mapping.select_next_item()";
           "<C-b>" = "cmp.mapping.scroll_docs(-4)";
+          "<PageUp>" = "cmp.mapping.scroll_docs(-4)";
           "<C-f>" = "cmp.mapping.scroll_docs(4)";
+          "<PageDown>" = "cmp.mapping.scroll_docs(4)";
           "<C-Space>" = "cmp.mapping.complete()";
-          "<C-c>" = "cmp.mapping.abort()";
-          "<CR>" = "cmp.mapping.confirm({ select = true, behavior = cmp.ConfirmBehavior.Replace })";
+          "<LeftMouse>" = "cmp.mapping.complete()";
+          "<C-e>" = "cmp.mapping.abort()";
+          "<Esc>" = "cmp.mapping.abort()";
+          "<CR>" = "cmp.mapping.confirm({ select = false })";
         };
         sources = {
           "nvim_lsp" = {
