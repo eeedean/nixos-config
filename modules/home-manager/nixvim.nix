51f28c99b8bda339a749ac6f32ae7d21a8b2c857
Dean Eckert <dean.eckert@red-oak-consulting.com>
2026-02-01 00:19:42 +0100

    feat(nixvim): added treesitter highlight

--- modules/home-manager/nixvim.nix
+++ modules/home-manager/nixvim.nix
@@ -46,9 +46,13 @@
       tagbar.enable = true;
       treesitter = {
         enable = true;
+        highlight.enable = true;
         indent = {
           enable = true;
         };
+      };
+      treesitter-context = {
+        enable = true;
       };
 
       gitsigns = {
